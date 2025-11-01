8d5be5c517358d48b82f7fcaf0250c9a75875158
Ruslan <arnov@i.ua>
2025-11-01 15:42:16 +0200

    Parsec 43

--- src/js/add-edit-delete-developer.js
+++ src/js/add-edit-delete-developer.js
@@ -4,10 +4,17 @@ console.error('Додаємо/Редагуємо/Видаляємо розроб
 const addDeveloperButton = document.querySelector(".our-developers__add-button");
 const developersList = document.querySelector(".our-developers-list");
 const modalAddEditDeveloper = document.querySelector("[data-modal-add-edit-developer]");
+const formTitle = document.querySelector(".form-add-edit-developer__title");
+
+//todo: Кнопки модальних вікон
+const btnAddEditDeveloper = document.querySelector(".form-add-edit-developer__btn-add-edit-developer");
+const btnDeleteDeveloper = document.querySelector("[data-btn-delete-developer]");
+const btnCancelDeveloper = document.querySelector("[data-btn-cancel-developer]");
 
 //! Додаємо слухачів до знайдених елементів
 addDeveloperButton.addEventListener("click", addDeveloper);
 developersList.addEventListener("click", editDeleteDeveloper);
+btnCancelDeveloper.addEventListener("click", toggleModalAddEditDeveloper);
 
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -21,9 +28,14 @@ function toggleModalAddEditDeveloper() {
 //! Функція ДОДАЄ нових розробників
 function addDeveloper() {
     console.log("ДОДАЄМО нового розробника");
-    //! ВІДКРИИВАЄМО модальне вікно з формою для ДОДАВАННЯ/РЕДАГУВАННЯ/+ВИДАЛЕННЯ
+    //todo: ВІДКРИИВАЄМО модальне вікно з формою для ДОДАВАННЯ/РЕДАГУВАННЯ/+ВИДАЛЕННЯ
     toggleModalAddEditDeveloper();
-
+    //todo: Зміна контенту заголовка модального вікна
+    formTitle.textContent = "Додати розробника"
+    //todo: Зміна контенту кнопки "submit"
+    btnAddEditDeveloper.textContent = "Додати"
+    //todo: Приховуємо елементи:
+    btnDeleteDeveloper.style.display = "none";
     
 };
 
@@ -33,11 +45,16 @@ function editDeleteDeveloper(event) {
         return;
     };
     console.log("РЕДАГУЄМО/ВИДАЛЯЄМО розробників");
-
     // console.log("event.target", event.target);
     // console.log("event.currentTarget:", event.currentTarget);
     // console.log("event.target.nodeName", event.target.nodeName);
 
     //! ВІДКРИВАЄМО модальне вікно з формою для РЕДАГУВАННЯ/ВИДАЛЕННЯ
     toggleModalAddEditDeveloper();
+    //todo: Зміна контенту заголовка модального вікна
+    formTitle.textContent = "Редагувати дані розробника"
+    //todo: Зміна контенту кнопки "submit"
+    btnAddEditDeveloper.textContent = "Редагувати"
+    //! Повертаємо елементи:
+    btnDeleteDeveloper.style.display = "block";
 };
